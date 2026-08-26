4962ed131b4b9cad6dfa419fa9e7432106781fc8
kyrshopecu-lgtm <kyrshopecu@gmail.com>
2026-08-25 19:29:33 -0500

    Point admin panel to same-origin production API

--- admin-web/config.js
+++ admin-web/config.js
@@ -1,5 +1,5 @@
 window.GOY_ADMIN_CONFIG = {
   mode: 'api',
-  apiBaseUrl: 'http://localhost:8787',
-  registrationBaseUrl: 'http://localhost:8080/registro.html'
+  apiBaseUrl: '/api',
+  registrationBaseUrl: `${window.location.origin}/registro`
 };
