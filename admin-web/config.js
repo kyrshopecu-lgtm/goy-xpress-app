df1b735e3974ac11c64a03fa0ea57380bcf2d031
kyrshopecu-lgtm <kyrshopecu@gmail.com>
2026-08-25 19:14:27 -0500

    chore: add admin web configuration

---
+++ admin-web/config.js
@@ -0,0 +1,9 @@
+window.GOY_ADMIN_CONFIG = {
+  mode: 'demo',
+  apiBaseUrl: '',
+  registrationBaseUrl: 'https://goyxpress.com/registro',
+  demoAdmin: {
+    email: 'admin@goyxpress.com',
+    password: 'Cambiar123!'
+  }
+};
