b37669e5441fb626367aebe2b1a9c0b61068dba0
kyrshopecu-lgtm <kyrshopecu@gmail.com>
2026-09-01 20:40:11 -0500

    API: conectar perfil y registro ampliado de mensajeros

--- api/[...path].js
+++ api/[...path].js
@@ -1,5 +1,6 @@
 const baseHandler = require('../server/server-v6');
 const {wrap:wrapClients} = require('../server/admin-clients');
 const {wrap:wrapManagement} = require('../server/admin-management');
+const {wrap:wrapCourierProfile} = require('../server/courier-profile');
 
-module.exports = wrapManagement(wrapClients(baseHandler));
+module.exports = wrapCourierProfile(wrapManagement(wrapClients(baseHandler)));
